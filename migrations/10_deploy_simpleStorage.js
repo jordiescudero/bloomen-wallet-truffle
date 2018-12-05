09b436ed7773b46ecc6b9edb71c1e2fb0f732572
Jordi Escudero <jordi.escudero@gmail.com>
2018-12-05 09:45:29 +0100

    new contracts

--- migrations/10_deploy_simpleStorage.js
+++ migrations/10_deploy_simpleStorage.js
@@ -1,6 +1,6 @@
 var SimpleStorage = artifacts.require("./utils/SimpleStorage.sol");
 
  module.exports = function(deployer) {
-//   deployer.deploy(SimpleStorage,34);
+   deployer.deploy(SimpleStorage,34);
  };
 
