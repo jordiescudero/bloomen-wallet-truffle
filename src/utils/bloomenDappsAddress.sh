1f8abe343fe7cc2fedb4ec22a7610db1078ba776
Jordi Escudero <jordi.escudero@gmail.com>
2019-01-11 12:32:11 +0100

    more dapps

--- src/utils/bloomenDappsAddress.sh
+++ src/utils/bloomenDappsAddress.sh
@@ -1,2 +1,16 @@
 node src/contractUtilsBoomen.js add 0x24Af9351Eb5c8dD6b684861281657AB90A37d454
 node src/contractUtilsBoomen.js add 0x1309a2521cD0990dF7cD793fafAd204094efA8E8
+
+
+blue 
+node src/contractUtilsBoomen.js add 0x63271380bB3F6E6ab65A4Ac556874e8efff32108
+brown
+node src/contractUtilsBoomen.js add  0xc7A351328cA61f3ACfCcdc5E3b28604F14aDE988
+green
+node src/contractUtilsBoomen.js add  0x821EA16c6A9556FAd4d3F8a6abB63BcbEEb88118
+grey
+node src/contractUtilsBoomen.js add  0x09626ddBF657a0BF4A980eF01C165b417eAE910F
+lilac
+node src/contractUtilsBoomen.js add  0x6Ae033bbC33f5b0cE6BF2ccb8e2a81160052dFB7
+orange
+node src/contractUtilsBoomen.js add  0x2A329593C5b6882f15beF3266A738Faf285F3A37
